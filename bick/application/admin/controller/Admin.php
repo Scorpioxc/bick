6b195a2c55a0442f1d48733df8ccbfb500a44d6e
Scropioxc <913317612@qq.com>
2019-04-17 17:53:02 +0800

    使用模型对象实现管理员查询

--- bick/application/admin/controller/Admin.php
+++ bick/application/admin/controller/Admin.php
@@ -1,16 +1,57 @@
 <?php
 namespace app\admin\controller;
-class Admin 
+use think\Controller;
+use app\admin\model\Admin as AdminModel;
+class Admin extends Controller
 {
+    /* $admin = 1;
+    public function _initialize()
+    {
+        global $admin = new AdminModel();
+    }*/
+
+
     public function lst()
-    { 
-	  return view();
+    {
+        //$res = db('admin')->find();
+        //$res = db('admin')->find(2);
+        //$res = db('admin')->select();
+        //$res = db('admin')->field('name')->select();
+        //$res = db('admin')->where('id',1)->select();
+        //$res = db('admin')->where(array('id'=>2,'name'=>1))->select();
+        //$admin = new AdminModel();
+        //$res = $admin->select();
+        //$res = AdminModel::where('name',1)->order('id','desc')->column('id,name,password');
+       /* $res = AdminModel::getByName(1);
+        dump($res);die;
+        foreach ($res as $key => $value){
+            echo $value -> password;
+            echo "<br>";
+        }*/
+        $admin=new AdminModel();
+        $res=$admin->getAdmin();
+        //dump($res);die;
+        $this->assign('res',$res);
+        //dump($res);
+
+	    return view();
     }
 	
 	public function add()
     { 
-        //dump($_POST);die;
-	  return view();
+
+        if(request()->isPost()){
+
+            $admin = new AdminModel();
+
+            if($admin->addAdmin(input('post.'))){
+                $this->success('添加管理员成功!',url('lst'));
+            }else{
+                $this->error('添加管理员失败!');
+            }
+            return;
+        }
+        return view();
     }
 	
 	public function edit()
---
+++ bick/application/admin/model/Admin.php
@@ -0,0 +1,32 @@
+<?php
+namespace app\admin\model;
+use think\Model;
+class Admin extends Model
+{
+
+	
+	public function addAdmin($data)
+    { 
+        if (empty($data) || !is_array($data)){
+            return false;
+        }
+        if ($data['password']){
+            $data['password'] = md5($data['password']);
+        }
+
+        $res = $this->save($data);
+        //dump($res);die;
+        if ($res){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public function getAdmin()
+    {
+        return $this::select();
+    }
+
+
+}
