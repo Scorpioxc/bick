d9add68b93c32b192885aa4112d46f6e1daaaf22
Scropioxc <913317612@qq.com>
2019-04-22 19:33:57 +0800

    后台管理新增文章

--- bick/application/admin/controller/Article.php
+++ bick/application/admin/controller/Article.php
@@ -2,7 +2,7 @@
 namespace app\admin\controller;
 use app\admin\model\Article as ArticleModel;
 use app\admin\model\Cate as CateModel;
-
+use think\Request;
 class Article extends Common
 {
 
@@ -10,11 +10,7 @@ class Article extends Common
     public function lst()
     {
 
-        if(request()->isPost()){
-            dump(input('post.'));die;
 
-            return;
-        }
         $articleRes=db('article')->field('a.*,b.catename')->alias('a')->join('bk_cate b','a.cateid=b.id')->order('a.id desc')->paginate(5);
         //$this->assign('artres',$artres);
         //dump($res);die;
@@ -24,13 +20,30 @@ class Article extends Common
     }
 	
 	public function add()
-    { 
+    {
+        $article = new ArticleModel();
 
         if(request()->isPost()){
+            $data = input('post.');
+        /*//dump($_FILES['thumb']);die;
+            $data = input('post.');
+            if (!$_FILES['thumb']['error']>0){
+                $file = request()->file('thumb');
 
-            $article = new ArticleModel();
+                $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads');
+                if ($info){
+                    $thumb = 'http://www.bick1.com' . DS . 'uploads'.DS.$info->getSaveName();
+                    //dump($thumb);die;
+                    //$this->success('图片上传成功',url('lst'));
+                    $data['thumb'] = $thumb;
+                }else{
+                    $this->error('图片上传失败');
+                }
+            }*/
 
-            if($article->save(input('post.'))){
+            //dump($_FILES['thumb']['error']);die;
+
+            if($article->save($data)){
                 $this->success('添加文章成功！',url('lst'));
             }else{
                 $this->error('添加文章失败!');
@@ -45,7 +58,25 @@ class Article extends Common
 	
 	public function edit($id)
     {
+        if(request()->isPost()){
+            $data=input('post.');
 
+            $article=new ArticleModel;
+            $save=$article->update($data);
+            if($save){
+                $this->success('修改文章成功！',url('lst'));
+            }else{
+                $this->error('修改文章失败！');
+            }
+            return;
+        }
+        $cate=new CateModel();
+        $cateres=$cate->cateRes();
+        $arts=db('article')->where(array('id'=>input('id')))->find();
+        $this->assign(array(
+            'cateres'=>$cateres,
+            'arts'=>$arts,
+        ));
         return view();
     }
 
--- bick/application/admin/model/Article.php
+++ bick/application/admin/model/Article.php
@@ -4,7 +4,51 @@ use think\Model;
 class Article extends Model
 {
 
+    protected static function init()
+    {
+        Article::event('before_insert',function($article){
+            if($_FILES['thumb']['tmp_name']){
+                $file = request()->file('thumb');
+                $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads');
+                if($info){
+                    // $thumb=ROOT_PATH . 'public' . DS . 'uploads'.'/'.$info->getExtension();
+                    $thumb='http://www.bick1.com' . DS . 'uploads'.'/'.$info->getSaveName();
+                    $article['thumb']=$thumb;
+                }
+            }
+        });
 
+
+        Article::event('before_update',function($article){
+            if($_FILES['thumb']['tmp_name']){
+                $arts=Article::find($article->id);
+                $thumbpath=$arts['thumb'];
+                dump(file_exists($thumbpath));die;
+                if(file_exists($thumbpath)){
+                    @unlink($thumbpath);
+                }
+                $file = request()->file('thumb');
+                $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads');
+                if($info){
+                    // $thumb=ROOT_PATH . 'public' . DS . 'uploads'.'/'.$info->getExtension();
+                    $thumb='http://www.bick1.com' . DS . 'uploads'.'/'.$info->getSaveName();
+                    $article['thumb']=$thumb;
+                }
+
+            }
+        });
+
+        Article::event('before_delete',function($article){
+
+            $arts=Article::find($article->id);
+            $thumbpath=$_SERVER['DOCUMENT_ROOT'].$arts['thumb'];
+            if(file_exists($thumbpath)){
+                @unlink($thumbpath);
+            }
+        });
+
+
+    }
 
 
 }
